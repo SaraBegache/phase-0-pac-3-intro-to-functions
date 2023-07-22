cacefc94a7aea83e310fda1f32f2b1507f552a56
Sara BEGACHE <sara.begache@gmail.com>
2023-07-22 16:31:49 +0100

    Practicing functions in js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Follow along with the examples here
+// function doNothing () {}
+// function sayHello () {
+//     console.log("Hello!");
+// }
+// sayHello();
+
+// function sayHelloToGuadalupe () {
+//     console.log("Hello, Guadalupe");
+// }
+// sayHelloToGuadalupe();
+
+// function sayHelloToLiz() {
+//     console.log("Hello, Liz!");
+//   }
+// sayHelloToLiz();
+
+// function sayHelloToSamip() {
+//     console.log("Hello, Samip!");
+//   }
+//   sayHelloToSamip();
+
+//   sayHelloToGuadalupe();
+//   sayHelloToLiz();
+//   sayHelloToSamip();
+
+// function sayHelloTo(firstName) {
+//     console.log(`Hello, ${firstName}!`);
+// }
+
+// sayHelloTo("Guadalupe");
+// sayHelloTo("Jane");
+// sayHelloTo("Me");
+// sayHelloTo(1);
+
+// console.log(firstName);
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+//   }
+//   say("Julio", "Goodbye");
+function add(x, y) {
+    return x + y;
+}
